37926f7d2a04a92b4bdef938fc04e3c0b97f473d
hanishkvc <hanishkvc@gmail.com>
2020-06-26 13:41:31 +0530

    Added the initial skeleton for fbdetile video filter
    Duplicated vf_edgedetect and modified to get fbdetile skeleton.
    
    Added the initial skeletal flow for a simple and dumb way of detiling
    intelx based framebuffer layout into a linear layout.
    
    This is currently hardcoded for 32bit / pixel based tiles (for certain
    calculations), as well as assumes that the source linesize matches
    (the width*4) so doesnt handle any pitch accomodating for src pointer.
    
    Updated Makefile and allfilters to link in the new filter.

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -223,6 +223,7 @@ extern AVFilter ff_vf_drawgraph;
 extern AVFilter ff_vf_drawgrid;
 extern AVFilter ff_vf_drawtext;
 extern AVFilter ff_vf_edgedetect;
+extern AVFilter ff_vf_fbdetile;
 extern AVFilter ff_vf_elbg;
 extern AVFilter ff_vf_entropy;
 extern AVFilter ff_vf_eq;
