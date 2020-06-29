915c330944824dddf09e7d1ede7738eff76164f6
hanishkvc <hanishkvc@gmail.com>
2020-06-29 20:11:37 +0530

    TileYf walk pattern generation
    Moved the logic into function.
    
    With this trigger both the optimised directionChangeList (smaller)
    as well as the raw directionChangeList (longer) based walking.
    
    Also for the optimised directionChangeList, I had swapped the x and
    y values for the last two changes, that has been fixed now.

--- hkvc/hkvc-tile-check.py
+++ hkvc/hkvc-tile-check.py
@@ -4,26 +4,59 @@
 
 import matplotlib.pyplot as plt
 
-#check=[ [8, 4, 0], [16,8,-4], [32,-8,4] ]
-check=[[4, 0, 4], [8, 4,-4], [16,-4,4], [32,4,-12]]
-plt.plot([0,50,100,150,200],[0,50,100,150,200])
-x,y = 0,0
-me = 0
-for i in range(1024):
-    if i == 0:
-        plt.text(0,0,me)
-        me += 1
-        continue
-    for k in range(len(check)):
-        if (i%check[-1-k][0]) == 0:
-            x += check[-1-k][1]
-            y += check[-1-k][2]
-            if (x >= 200):
-                x = 0
-                y += 16
-            print(check[-1-k][0],x,y,me)
-            plt.text(x,y,me)
+
+def tileyf_t1():
+    check=[[4, 0, 4], [8, 4,-4], [16,-4,4], [32,4,-12]]
+    plt.plot([0,50,100,150,200],[0,50,100,150,200])
+    x,y = 0,0
+    me = 0
+    for i in range(1024):
+        if i == 0:
+            plt.text(0,0,me)
             me += 1
-            break
-plt.show()
+            continue
+        for k in range(len(check)):
+            if (i%check[-1-k][0]) == 0:
+                x += check[-1-k][1]
+                y += check[-1-k][2]
+                if (x >= 200):
+                    x = 0
+                    y += 16
+                print(check[-1-k][0],x,y,me)
+                plt.text(x,y,me)
+                me += 1
+                break
+    plt.show()
+
+
+def tileyf_t2():
+    check=[ [8, 4, 0], [16,-4,8], [32,4,-8] ]
+    plt.plot([0,50,100,150,200],[0,50,100,150,200])
+    x,y = 0,0
+    me = 0
+    for i in range(1024):
+        if i == 0:
+            plt.text(0,0,me)
+            plt.text(0,4,me+1)
+            me += 2
+            continue
+        for k in range(len(check)):
+            if (i%check[-1-k][0]) == 0:
+                x += check[-1-k][1]
+                y += check[-1-k][2]
+                if (x >= 200):
+                    x = 0
+                    y += 16
+                print(check[-1-k][0],x,y,me)
+                plt.text(x,y,me)
+                plt.text(x,y+4,me+1)
+                me += 2
+                break
+    plt.show()
+
+
+
+tileyf_t2()
+
+
 
