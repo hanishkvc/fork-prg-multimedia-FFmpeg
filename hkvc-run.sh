10b0e043c16b3d55229a636414733344d119e15b
hanishkvc <hanishkvc@gmail.com>
2020-06-29 12:28:58 +0530

    Cleaned up hkvc-run.sh helper script

--- hkvc-run.sh
+++ hkvc-run.sh
@@ -27,19 +27,24 @@ function _build_configure() {
 function _time_ffmpeg() {
 	echo -e "\n**** $1 ****\n"
 	totalTime=0
+	totalTSC=0
 	for i in 1 2 3 4; do
 		#rm test.mp4; time ./ffmpeg -i ../teststreams/testintelxtile.mp4 $1 test.mp4 2> /tmp/hkvc-run.log
 		bash -c "time ./ffmpeg -y -i ../teststreams/testintelxtile.mp4 $1 -f h264 /dev/null" 2> /tmp/hkvc-run.log
 		fM=`grep "real" /tmp/hkvc-run.log | cut -f 2 | cut -d 'm' -f 1`
 		fS=`grep "real" /tmp/hkvc-run.log | cut -f 2 | cut -d 'm' -f 2 | cut -d 's' -f 1`
 		grep "real" /tmp/hkvc-run.log
-		grep "perf" /tmp/hkvc-run.log
-		#curTime=$(($fM*60+$fS))
 		curTime=`math "$fM*60*100+$fS*100"`
 		totalTime=$(($totalTime+$curTime))
+		grep "perf" /tmp/hkvc-run.log
+		curTSC=`grep "perf" /tmp/hkvc-run.log | cut -d ' ' -f 3`
+		if [ "$curTSC" != "" ]; then
+			totalTSC=$(($totalTSC+$curTSC))
+		fi
 	done
 	AvgTime=`math -f "$totalTime/(4*100)"`
-	echo "AvgTime=$AvgTime"
+	AvgTSC=`math -f "$totalTSC/4"`
+	echo "AvgTime=$AvgTime, AvgTSC=$AvgTSC"
 }
 
 function time_ffmpeg() {
