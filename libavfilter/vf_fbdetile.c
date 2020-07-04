745252526aaba68c00c532367a51cad3532b43fa
hanishkvc <hanishkvc@gmail.com>
2020-07-04 11:23:11 +0530

    fbdetile gets supported pixformats from fbtile

--- libavfilter/vf_fbdetile.c
+++ libavfilter/vf_fbdetile.c
@@ -139,14 +139,9 @@ static av_cold int init(AVFilterContext *ctx)
 
 static int query_formats(AVFilterContext *ctx)
 {
-    // Currently only RGB based 32bit formats are specified
-    // TODO: Technically the logic is transparent to 16bit RGB formats also to a great extent
-    static const enum AVPixelFormat pix_fmts[] = {AV_PIX_FMT_RGB0, AV_PIX_FMT_0RGB, AV_PIX_FMT_BGR0, AV_PIX_FMT_0BGR,
-                                                  AV_PIX_FMT_RGBA, AV_PIX_FMT_ARGB, AV_PIX_FMT_BGRA, AV_PIX_FMT_ABGR,
-                                                  AV_PIX_FMT_NONE};
     AVFilterFormats *fmts_list;
 
-    fmts_list = ff_make_format_list(pix_fmts);
+    fmts_list = ff_make_format_list(fbtilePixFormats);
     if (!fmts_list)
         return AVERROR(ENOMEM);
     return ff_set_common_formats(ctx, fmts_list);
--- libavutil/fbtile.c
+++ libavutil/fbtile.c
@@ -60,9 +60,12 @@ int fbtilemode_from_formatmodifier(uint64_t formatModifier)
 
 /**
  * Supported pixel formats
+ * Currently only RGB based 32bit formats are specified
+ * TODO: Technically the logic is transparent to 16bit RGB formats also to a great extent
  */
 const enum AVPixelFormat fbtilePixFormats[] = {AV_PIX_FMT_RGB0, AV_PIX_FMT_0RGB, AV_PIX_FMT_BGR0, AV_PIX_FMT_0BGR,
-                                               AV_PIX_FMT_RGBA, AV_PIX_FMT_ARGB, AV_PIX_FMT_BGRA, AV_PIX_FMT_ABGR, AV_PIX_FMT_NONE};
+                                               AV_PIX_FMT_RGBA, AV_PIX_FMT_ARGB, AV_PIX_FMT_BGRA, AV_PIX_FMT_ABGR,
+                                               AV_PIX_FMT_NONE};
 
 int fbtile_checkpixformats(const enum AVPixelFormat srcPixFormat, const enum AVPixelFormat dstPixFormat)
 {
